a53ff7ac42e1b7f2e5686b644b3213aa6291d66a
GoGadAFI <neeluofficial369@gmail.com>
2026-08-25 06:24:08 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "gogad-expense-v1";
+
+const FILES_TO_CACHE = [
+  "/expense-tracker/",
+  "/expense-tracker/index.html",
+  "/expense-tracker/manifest.json",
+  "/expense-tracker/icon-192.png",
+  "/expense-tracker/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
